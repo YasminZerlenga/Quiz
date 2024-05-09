4f85bc12804a53a86cc608062c1a2f3cc19b42f0
YasminZerlenga <Yasminzerleng@gmail.com>
2024-05-09 13:51:28 -0300

    'v.01'

---
+++ Quiz.js
@@ -0,0 +1,43 @@
+export default [
+
+    {
+        question: "Qual é a linguagem de programação mais popular em 2021?",
+        answers: [
+            { option: "JavaScript", correct: true },
+            { option: "Python", correct: false},
+            { option: "Java", correct: false},
+        ],
+    },
+    {
+        question: "Qual empresa é responsável pelo sistema operacional Windows?",
+        answers: [
+            { option: "Apple", correct: false},
+            { option: "Microsoft", correct: true},
+            { option: "Google", correct: false},
+        ],
+    },
+    {
+        question: "Qual é o protocolo de transferência de hipertexto usado para enviar e receber páginas da web?",
+        answers: [
+            { option: "HTTP", correct: true},
+            { option: "FTP", correct: false},
+            { option: "SMTP", correct: false},
+        ],
+    },
+    {
+        question: "O que é um 'array' em programação?",
+        answers: [
+            { option: "Um tipo de função que retorna um valor específico", correct: false},
+            { option: "Um tipo de variável que pode armazenar apenas valores verdadeiros ou falsos", correct: false},
+            { option: "Uma estrutura de dados que armazena uma coleção de elementos do mesmo tipo", correct: true},
+        ],
+    },
+    {
+        question: "O que é uma 'função' em programação?",
+        answers: [
+            { option: "Um bloco de código que executa uma tarefa específica e pode ser reutilizado em diferentes partes de um programa", correct: true},
+            { option: "Uma variável que armazena múltiplos valores em uma única estrutura", correct: false},
+            { option: "Uma instrução que permite que o programa faça uma escolha entre diferentes caminhos de execução", correct: false},
+        ],
+    },
+];
